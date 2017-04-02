b254f2303e7da4fde469041f520cd6517529db07
daun335723 <daun335723@LT-15-578>
2017-04-03 02:29:28 +0800

    Add project files.

---
+++ ExperimentMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExperimentMVC.MvcApplication" Language="C#" %>
---
+++ ExperimentMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExperimentMVC.Startup))]
+namespace ExperimentMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
